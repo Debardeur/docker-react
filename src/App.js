b00598e8a96a0c91a70c2041873289d6815f9be4
hnaanani <hnaanani@octo.com>
2019-11-18 11:22:34 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    stp
+                    C H A N G E D
                 </p>
                 <a
                     className="App-link"
